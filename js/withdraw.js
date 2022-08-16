2dc3d7f586a0786f89e1693c260a84be3e9647f0
Sajia09 <sajiaa09@gmail.com>
2022-08-16 16:44:04 +0600

    first commit

---
+++ js/withdraw.js
@@ -0,0 +1,28 @@
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+    withdrawField.value = '';  
+
+    if(newWithdrawAmount > previousBalanceTotal)
+    {
+        alert("Baap er bank e eto tk nai");
+        return;
+    }
+
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+    
+    const currentBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = currentBalanceTotal;
+    
+
+})
